82bb14895e774c59611ae3a190accb5d241a0996
Jean-Luc DOH <freehzaix@gmail.com>
2023-10-14 11:59:55 +0000

    Sauvegarde du 14/10/2023

---
+++ resources/views/layouts/about.blade.php
@@ -0,0 +1,31 @@
+<div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-7">
+                <div class="section-title mb-4">
+                    <h5 class="position-relative d-inline-block text-primary text-uppercase">A propos</h5>
+                    <h1 class="display-5 mb-0">Boostez votre présence en ligne avec notre agence de marketing digital experte</h1>
+                </div>
+                <h4 class="text-body fst-italic mb-4">Boostez votre entreprise avec notre expertise en marketing digital. Maximisez votre croissance grâce à des stratégies personnalisées, une visibilité accrue et une optimisation des canaux digitaux pour atteindre vos objectifs commerciaux ambitieux.</p>
+                <div class="row g-3">
+                    <div class="col-sm-6 wow zoomIn" data-wow-delay="0.3s">
+                        <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>Expérience et expertise</h5>
+                        <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>Résultats mesurables</h5>
+                        <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>Approche stratégique</h5>
+                    </div>
+                    <div class="col-sm-6 wow zoomIn" data-wow-delay="0.6s">
+                        <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>Créativité et innovation</h5>
+                        <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>Communication transparente</h5>
+                        <h5 class="mb-3"><i class="fa fa-check-circle text-primary me-3"></i>Veille technologique</h5>
+                    </div>
+                </div>
+                <a href="{{ route('about') }}" class="btn btn-primary py-3 px-5 mt-4 wow zoomIn" data-wow-delay="0.6s">Qui sommes-nous ?</a>
+            </div>
+            <div class="col-lg-5" style="min-height: 500px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s" src="img/about.jpg" style="object-fit: cover;">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
